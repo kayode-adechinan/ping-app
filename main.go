b800e40b40dfc60873069228b0479329dd89adfb
Kayode Adechinan <kayode.adechinan@gmail.com>
2024-03-14 12:13:08 +0000

    updates

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello!")
+		fmt.Fprintf(w, "Pingged!")
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
